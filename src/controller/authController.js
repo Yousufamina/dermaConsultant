3f1b4c8c2ee70eb35bfe936d3ad5a4197c13a5f3
yousufamina <amina.yousuf@prime-logics.com>
2025-03-23 21:41:13 +0500

    update api

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -379,8 +379,6 @@ export const deleteUser = async (req, res) => {
         res.status(200).json({
           message: 'User deleted successfully'
         })
-              
-
   } catch (error) {
       console.error('Error deleting user:', error);
       res.status(500).json({ message: 'Server error' });
